b5dd3d50ce0b163c2282292e1d68eed5185fec20
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-11-07 01:44:44 -0500

    story page allf features working now\

--- lib/presentation/pages/story_page.dart
+++ lib/presentation/pages/story_page.dart
@@ -1,4 +1,5 @@
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -36,10 +37,8 @@ class _StoryPageState extends State<StoryPage> {
       child: Scaffold(
         body: Container(
           color: Colors.black,
-          child: Center(
-            child: _buildStoryView(
-              storyState,
-            ),
+          child: _buildStoryView(
+            storyState,
           ),
         ),
       ),
@@ -49,26 +48,56 @@ class _StoryPageState extends State<StoryPage> {
   Widget _buildStoryView(StoryState state) {
     return Stack(
       children: [
-        _builStoryPicture(
-          state: state,
-        )
+        Center(
+          child: _builStoryPicture(state: state),
+        ),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildBttn(
+                Icons.arrow_back_ios_rounded,
+                onPressed: () {
+                  if (_pageController.page! <
+                      state.stories[state.userId]!.length - 1) {
+                    _pageController.previousPage(
+                      duration: const Duration(milliseconds: 1),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+              ),
+              _buildBttn(
+                Icons.arrow_forward_ios_rounded,
+                onPressed: () {
+                  if (_pageController.page! <
+                      state.stories[state.userId]!.length - 1) {
+                    _pageController.nextPage(
+                        duration: const Duration(milliseconds: 1),
+                        curve: Curves.easeInOut);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
 
-  Widget _buildBttn(StoryState state) {
-    return IconButton(
-      onPressed: () {
-        if (_pageController.page! < state.stories[state.userId]!.length - 1) {
-          _pageController.previousPage(
-              duration: const Duration(milliseconds: 1),
-              curve: Curves.easeInOut);
-        }
-      },
-      icon: const CircleAvatar(
-        child: Icon(
+  Widget _buildBttn(
+    IconData icon, {
+    required void Function()? onPressed,
+  }) {
+    return CircleAvatar(
+      backgroundColor: const Color.fromARGB(173, 92, 92, 92),
+      radius: 35,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(
           color: Colors.white,
-          Icons.arrow_back_ios_rounded,
+          icon,
+          size: 25,
         ),
       ),
     );
@@ -77,28 +106,70 @@ class _StoryPageState extends State<StoryPage> {
   Widget _builStoryPicture({
     required StoryState state,
   }) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: AspectRatio(
-        aspectRatio: 9 / 16,
-        child: PageView.builder(
-          controller: _pageController,
-          scrollDirection: Axis.horizontal,
-          onPageChanged: (index) {
-            setState(() {
-              _currentPageIndex = index;
-            });
-          },
-          itemCount: state.stories[state.userId]!.length,
-          itemBuilder: (BuildContext context, int index) {
-            final story = state.stories[state.userId]![index];
-            return Image.memory(
-              story.image!,
-              fit: BoxFit.cover,
-            );
-          },
+    return Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: AspectRatio(
+            aspectRatio: 9 / 16,
+            child: PageView.builder(
+              controller: _pageController,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
+              },
+              itemCount: state.stories[state.userId]!.length,
+              itemBuilder: (BuildContext context, int index) {
+                final story = state.stories[state.userId]![index];
+                return Image.memory(
+                  story.image!,
+                  fit: BoxFit.cover,
+                );
+              },
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          bottom: 10,
+          left: 0,
+          right: 0,
+          child: state.stories[state.userId]!.isNotEmpty
+              ? Container(
+                  height: 50,
+                  color: const Color.fromARGB(154, 0, 0, 0),
+                  child: DotsIndicator(
+                    position: _currentPageIndex,
+                    dotsCount: state.stories[state.userId]!.length,
+                    decorator: DotsDecorator(
+                      activeColor: Colors.white,
+                      activeSize: const Size(14, 14),
+                      color: Colors.grey[400]!,
+                      size: const Size(12, 12),
+                    ),
+                  ),
+                )
+              : Container(),
+        ),
+        Positioned(
+          top: 10,
+          right: 15,
+          child: IconButton(
+            onPressed: () =>
+                Navigator.pushReplacementNamed(context, Routes.indexPage),
+            icon: const Icon(
+              CupertinoIcons.xmark_square_fill,
+              size: 50,
+              shadows: [
+                BoxShadow(
+                    color: Colors.black, offset: Offset(0, 2), blurRadius: 5.0)
+              ],
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
